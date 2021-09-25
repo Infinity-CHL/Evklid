6896e1efe19712f6d98ac716206b87bfce8e4265
Mirjalilov Javohir <uzbekfarzandi906@gmail.com>
2021-09-25 18:46:53 +0500

    updated files

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoadede', function () {
-    document.querySelectorAll('.work-container__btn').forEach(function (tabsBtn) {
-        tabsBtn.addEventListener('click', function (event) {
-            const path = event.currentTarget.dataset.path
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.work-container__button').forEach(function (tabsBtn) {
+          tabsBtn.addEventListener('click', function (event) {
+             const path = event.currentTarget.dataset.path
 
-            document.querySelectorAll('work-content').forEach(function(tabContent) {
-                tabContent.classList.remove('.work-content-active')
-            })
-            document.querySelector(`[data-target="$(path)"]`).classList.add('work-content-active')
+             document.querySelectorAll('.tab-content').forEach(function(tabContent) {
+                 tabContent.classList.remove('tab-content-active')
+             })
+             document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
         })
     })
 })
